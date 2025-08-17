f0a6a8b9535a6dab5a09720b4468dbcd45b9cb15
safdar-azeem <safdarazeem00@gmail.com>
2025-08-17 13:59:18 +0500

    Add Icon.vue

---
+++ src/components/Icon.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { icons } from '../constants/icons'
+
+const props = defineProps<{
+   icon: string
+   width?: string
+   height?: string
+}>()
+
+const iconSvg = computed(() => {
+   return icons[props.icon]
+      .replace(/width="(\d+)"/, `width="${props?.width}"`)
+      .replace(/height="(\d+)"/, `height="${props?.height}"`)
+})
+</script>
+
+<template>
+   <div
+      :style="{
+         height: 'auto',
+         display: 'flex',
+      }"
+      v-html="iconSvg"></div>
+</template>
