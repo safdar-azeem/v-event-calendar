ba66bbe8041326231298c45d944a75569d324eff
safdar-azeem <safdarazeem00@gmail.com>
2025-08-17 13:59:18 +0500

    Add Scrollablar.vue

---
+++ src/components/Scrollablar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref, onBeforeUnmount } from 'vue'
+
+const isScrolling = ref(false)
+let timeoutId: any
+
+function handleScroll() {
+   isScrolling.value = true
+   clearTimeout(timeoutId)
+   timeoutId = setTimeout(() => {
+      isScrolling.value = false
+   }, 1000)
+}
+
+onBeforeUnmount(() => {
+   clearTimeout(timeoutId)
+})
+</script>
+
+<template>
+   <div class="scrollable-wrapper" :class="{ scrolling: isScrolling }" @scroll="handleScroll">
+      <slot />
+   </div>
+</template>
