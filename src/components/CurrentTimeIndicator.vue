b04d25948f1f99a8c1b0a4b69b9d090b5d17e81e
safdar-azeem <safdarazeem00@gmail.com>
2025-08-17 13:59:18 +0500

    Add CurrentTimeIndicator.vue

---
+++ src/components/CurrentTimeIndicator.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+   top: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+   <div class="current-time-indicator" :style="{ top }">
+      <div class="current-time-indicator__dot"></div>
+      <div class="current-time-indicator__line"></div>
+   </div>
+</template>
