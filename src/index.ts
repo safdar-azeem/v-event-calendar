2b0b86d07186725b0c00748b486a17ecd7732882
safdar-azeem <safdarazeem00@gmail.com>
2025-08-25 16:59:27 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
 import Calendar from './components/CalendarView.vue'
-import { extractTimeFromISO, extractDateFromISO } from './utils/calendarDateUtils'
+import {
+   extractTimeFromISO,
+   parseISOToDateTime,
+   extractDateFromISO,
+   formatToISO,
+   formatDate,
+   formatTime,
+   formatDisplayTime,
+   findNextAvailableTime,
+   getDuration,
+   hasTimeConflict,
+   addMinutes,
+} from './utils/calendarDateUtils'
 import type { CalendarEvent, CalendarView, CalendarViewConfig } from './types'
 
 export default Calendar
-export { extractTimeFromISO, extractDateFromISO, CalendarEvent, CalendarView, CalendarViewConfig }
+export {
+   extractTimeFromISO,
+   extractDateFromISO,
+   parseISOToDateTime,
+   formatDisplayTime,
+   findNextAvailableTime,
+   getDuration,
+   formatToISO,
+   formatDate,
+   formatTime,
+   hasTimeConflict,
+   addMinutes,
+   CalendarEvent,
+   CalendarView,
+   CalendarViewConfig,
+}
